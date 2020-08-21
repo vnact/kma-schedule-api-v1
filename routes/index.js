744f70e16d5487ca403cf84f963bd99ce5e9145b
Cuong Tran <clonebmn2itt@gmail.com>
2020-08-21 16:46:04 +0700

    chore: fix style

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,5 @@
-
-var express = require('express');
-var router = express.Router();
-/* GET home page. */
+const express = require('express');
+const router = express.Router();
 router.get('/', function (req, res, next) {
     res.render('index', { title: 'Trang chủ' });
 });
